f797739ab668835b418398f091b7b700155781b9
Nguyen Tu <tu.lqd.163@gmail.com>
2022-05-03 19:25:14 +0700

    Assigment CSOM

---
+++ ConsoleCSOM/Program.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SharePoint.Client;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.SharePoint.Client.Taxonomy;
+
+namespace ConsoleCSOM
+{
+    class SharepointInfo
+    {
+        public string SiteUrl { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                using (var clientContextHelper = new ClientContextHelper())
+                {
+                    ClientContext ctx = GetContext(clientContextHelper);
+                    ctx.Load(ctx.Web);
+                    await CreateList(ctx);
+                    Console.WriteLine($"Site {ctx.Web.Title}");
+
+                    //await SimpleCamlQueryAsync(ctx);
+
+                    //await CsomTermSetAsync(ctx);
+                }
+
+                Console.WriteLine($"Press Any Key To Stop!");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static ClientContext GetContext(ClientContextHelper clientContextHelper)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true);
+            IConfiguration config = builder.Build();
+            var info = config.GetSection("SharepointInfo").Get<SharepointInfo>();
+            return clientContextHelper.GetContext(new Uri(info.SiteUrl), info.Username, info.Password);
+        }
+
+        private static async Task GetFieldTermValue(ClientContext Ctx, string termId)
+        {
+            //load term by id
+            TaxonomySession session = TaxonomySession.GetTaxonomySession(Ctx);
+            Term taxonomyTerm = session.GetTerm(new Guid(termId));
+            Ctx.Load(taxonomyTerm, t => t.Labels,
+                                   t => t.Name,
+                                   t => t.Id);
+            await Ctx.ExecuteQueryAsync();
+        }
+
+        private static async Task CreateList(ClientContext ctx)
+        {
+            ListCreationInformation creationInfo = new ListCreationInformation();
+            creationInfo.Title = "ListTitle";
+            creationInfo.Description = "New list description";
+            creationInfo.TemplateType = (int)ListTemplateType.GenericList;
+            List newList = ctx.Web.Lists.Add(creationInfo);
+            ctx.Load(newList);
+            // Execute the query to the server.
+            await ctx.ExecuteQueryAsync();
+        }
+
+        private static async Task ExampleSetTaxonomyFieldValue(ListItem item, ClientContext ctx)
+        {
+            var field = ctx.Web.Fields.GetByTitle("fieldname");
+
+            ctx.Load(field);
+            await ctx.ExecuteQueryAsync();
+
+            var taxField = ctx.CastTo<TaxonomyField>(field);
+
+            taxField.SetFieldValueByValue(item, new TaxonomyFieldValue()
+            {
+                WssId = -1, // alway let it -1
+                Label = "correct label here",
+                TermGuid = "term id"
+            });
+            item.Update();
+            await ctx.ExecuteQueryAsync();
+        }
+
+        private static async Task CsomTermSetAsync(ClientContext ctx)
+        {
+            // Get the TaxonomySession
+            TaxonomySession taxonomySession = TaxonomySession.GetTaxonomySession(ctx);
+            // Get the term store by name
+            TermStore termStore = taxonomySession.GetDefaultSiteCollectionTermStore();
+            // Get the term group by Name
+            TermGroup termGroup = termStore.Groups.GetByName("Test");
+            // Get the term set by Name
+            TermSet termSet = termGroup.TermSets.GetByName("Test Term Set");
+
+            var terms = termSet.GetAllTerms();
+
+            ctx.Load(terms);
+            await ctx.ExecuteQueryAsync();
+        }
+
+        private static async Task CsomLinqAsync(ClientContext ctx)
+        {
+            var fieldsQuery = from f in ctx.Web.Fields
+                              where f.InternalName == "Test" ||
+                                    f.TypeAsString == "TaxonomyFieldTypeMulti" ||
+                                    f.TypeAsString == "TaxonomyFieldType"
+                              select f;
+
+            var fields = ctx.LoadQuery(fieldsQuery);
+            await ctx.ExecuteQueryAsync();
+        }
+
+        private static async Task SimpleCamlQueryAsync(ClientContext ctx)
+        {
+            var list = ctx.Web.Lists.GetByTitle("Documents");
+
+            var allItemsQuery = CamlQuery.CreateAllItemsQuery();
+            var allFoldersQuery = CamlQuery.CreateAllFoldersQuery();
+
+            var items = list.GetItems(new CamlQuery()
+            {
+                ViewXml = @"<View>
+                                <Query>
+                                    <OrderBy><FieldRef Name='ID' Ascending='False'/></OrderBy>
+                                </Query>
+                                <RowLimit>20</RowLimit>
+                            </View>",
+                FolderServerRelativeUrl = "/sites/test-site-duc-11111/Shared%20Documents/2"
+                //example for site: https://omniapreprod.sharepoint.com/sites/test-site-duc-11111/
+            });
+
+            ctx.Load(items);
+            await ctx.ExecuteQueryAsync();
+        }
+    }
+}
